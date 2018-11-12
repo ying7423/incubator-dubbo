84e0c680a2d440ae8bb9186a23a0b72f08e0942e
leixiuying <67894656@qq.com>
2018-11-12 22:52:36 +0800

    add comment

--- dubbo-filter/dubbo-filter-validation/src/main/java/org/apache/dubbo/validation/support/jvalidation/JValidator.java
+++ dubbo-filter/dubbo-filter-validation/src/main/java/org/apache/dubbo/validation/support/jvalidation/JValidator.java
@@ -240,33 +240,39 @@ public class JValidator implements Validator {
     @Override
     public void validate(String methodName, Class<?>[] parameterTypes, Object[] arguments) throws Exception {
         List<Class<?>> groups = new ArrayList<Class<?>>();
+        // 【第一种】添加以方法命名的内部接口，作为验证分组。例如 `ValidationService#save(...)` 方法，对应 `ValidationService.Save` 接口
         Class<?> methodClass = methodClass(methodName);
         if (methodClass != null) {
             groups.add(methodClass);
         }
+        // 验证错误集合
         Set<ConstraintViolation<?>> violations = new HashSet<ConstraintViolation<?>>();
         Method method = clazz.getMethod(methodName, parameterTypes);
         Class<?>[] methodClasses = null;
+        // 【第二种】添加方法的 @MethodValidated 注解的值对应的类，作为验证分组。
         if (method.isAnnotationPresent(MethodValidated.class)){
             methodClasses = method.getAnnotation(MethodValidated.class).value();
             groups.addAll(Arrays.asList(methodClasses));
         }
         // add into default group
+        // 【第三种】添加 Default.class 类，作为验证分组。在 JSR 303 中，未设置分组的验证注解，使用 Default.class 。
         groups.add(0, Default.class);
+        // 【第四种】添加服务接口类，作为验证分组。
         groups.add(1, clazz);
 
         // convert list to array
         Class<?>[] classgroups = groups.toArray(new Class[0]);
-
+        // 【第一步】获得方法参数的 Bean 对象。因为，JSR 303 是 Java Bean Validation ，以 Bean 为维度。
         Object parameterBean = getMethodParameterBean(clazz, method, arguments);
+        // 【第一步】验证 Bean 对象。
         if (parameterBean != null) {
             violations.addAll(validator.validate(parameterBean, classgroups ));
         }
-
+        // 【第二步】验证集合参数
         for (Object arg : arguments) {
             validate(violations, arg, classgroups);
         }
-
+        // 若有错误，抛出 ConstraintViolationException 异常。
         if (!violations.isEmpty()) {
             logger.error("Failed to validate service: " + clazz.getName() + ", method: " + methodName + ", cause: " + violations);
             throw new ConstraintViolationException("Failed to validate service: " + clazz.getName() + ", method: " + methodName + ", cause: " + violations, violations);
@@ -278,32 +284,44 @@ public class JValidator implements Validator {
         String methodClassName = clazz.getName() + "$" + toUpperMethoName(methodName);
         Class cached = methodClassMap.get(methodClassName);
         if (cached != null) {
+            //如果缓存值与clazz相同，则在获取类的时候异常，防止缓存击穿
             return cached == clazz ? null : cached;
         }
         try {
             methodClass = Class.forName(methodClassName, false, Thread.currentThread().getContextClassLoader());
             methodClassMap.put(methodClassName, methodClass);
         } catch (ClassNotFoundException e) {
+            //异常，将当前类存入缓存
             methodClassMap.put(methodClassName, clazz);
         }
         return methodClass;
     }
 
+    /**
+     * 验证集合参数
+     * @param violations
+     * @param arg
+     * @param groups
+     */
     private void validate(Set<ConstraintViolation<?>> violations, Object arg, Class<?>... groups) {
         if (arg != null && !isPrimitives(arg.getClass())) {
+            // [] 数组
             if (Object[].class.isInstance(arg)) {
                 for (Object item : (Object[]) arg) {
                     validate(violations, item, groups);
                 }
+            // 集合
             } else if (Collection.class.isInstance(arg)) {
                 for (Object item : (Collection<?>) arg) {
                     validate(violations, item, groups);
                 }
+            // map
             } else if (Map.class.isInstance(arg)) {
                 for (Map.Entry<?, ?> entry : ((Map<?, ?>) arg).entrySet()) {
                     validate(violations, entry.getKey(), groups);
                     validate(violations, entry.getValue(), groups);
                 }
+            // 验证单个元素
             } else {
                 violations.addAll(validator.validate(arg, groups));
             }
